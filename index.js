0ba748bb98fd9d27be11300ad45037b63161f67e
Roger Andersen <hexjelly@hexjelly.com>
2017-04-10 09:55:02 +0200

    add: add test data

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-let url = "http://elmaonline.net/API/times/374907?bestall=all&noorder=0&timeformat=hs"
+const ctx = require("canvas")
+const request = require("request")
+const fs = require("fs")
+
+// let levelId = 371127
+// let url = `http://elmaonline.net/API/times/${levelId}?bestall=all&noorder=0&timeformat=hs`
+
+// request(url, (error, response, body) => {
+//   console.log(body)
+// })
+
+let data = JSON.parse(fs.readFileSync('wc701_data.json', 'utf8'))
+Object.entries(data).forEach(([key, value]) => {
+  console.log(key, value)
+})
