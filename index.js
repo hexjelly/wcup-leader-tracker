e641955cea0f065490e11b061603342c8034daac
Roger Andersen <hexjelly@hexjelly.com>
2017-04-10 22:43:49 +0200

    add: produce first test results

--- index.js
+++ index.js
@@ -9,7 +9,31 @@ const fs = require("fs")
 //   console.log(body)
 // })
 
-let data = JSON.parse(fs.readFileSync('wc701_data.json', 'utf8'))
-Object.entries(data).forEach(([key, value]) => {
-  console.log(key, value)
+let data = Object.values(JSON.parse(fs.readFileSync('wc701_data.json', 'utf8')))
+
+data.forEach(val => {
+  val.datetime = new Date(val.datetime)
+  val.datetime.setHours(val.datetime.getHours() + 7)
 })
+
+data.sort((a, b) => {
+  return a.datetime - b.datetime
+})
+
+let deadline = new Date('2017-02-26 17:00')
+let leaders = []
+data.forEach(time => {
+  // before deadline
+  if (time.datetime.getTime() < deadline.getTime()) {
+    if (leaders.length > 0) {
+      // is this better than last leading time?
+      if (parseInt(time.time) < parseInt(leaders[leaders.length-1].time)) {
+        leaders.push({ name: time.kuski, time: parseInt(time.time), date: time.datetime })
+      }
+    } else { // empty
+      leaders.push({ name: time.kuski, time: parseInt(time.time), date: time.datetime })
+    }
+  }
+})
+
+console.log(leaders)
