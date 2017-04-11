b91515322364ec378475d9cb437841c85ea97a59
Roger Andersen <hexjelly@hexjelly.com>
2017-04-11 07:38:23 +0200

    add: highcharts sucks, but oh well, something

--- index.js
+++ index.js
@@ -64,46 +64,149 @@ function calculateStats(data, deadline) {
   return { leaders: leaders, durations: durations }
 }
 
+function hashColor(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash)
+  }
+  let colour = '#'
+  for (let i = 0; i < 3; i++) {
+    let value = (hash >> (i * 8)) & 0xFF
+    colour += ('00' + value.toString(16)).substr(-2)
+  }
+  return colour
+}
+
 function produceChart(data, startDate, endDate, options) {
   let startFormatted = Date.UTC(startDate.getUTCFullYear(), startDate.getUTCMonth(), startDate.getUTCDate(),
-    startDate.getUTCHours(), startDate.getUTCMinutes())
+    startDate.getUTCHours()-1, startDate.getUTCMinutes())
   let endFormatted = Date.UTC(endDate.getUTCFullYear(), endDate.getUTCMonth(), endDate.getUTCDate(),
     endDate.getUTCHours(), endDate.getUTCMinutes())
   let { leaders, durations } = calculateStats(data, endDate)
 
   let chart = {
-    chart: {
-      type: 'line'
-    },
-    // data: { dateFormat: 'YYYY-mm-dd' },
     title: {
       text: options.title || ''
     },
     series: [{
-      data: []
+      data: [],
+      lineWidth: 1,
+      showInLegend: false,
+      name: 'Time',
+      zoneAxis: 'x',
+        zones: []
     }],
     xAxis: {
       type: 'datetime',
-      tickInterval: 3600 * 1000 * 6,
+      tickInterval: 3600 * 1000 * 12,
       min: startFormatted,
       max: endFormatted,
+      title: { text: 'Date' }
+    },
+    yAxis: {
+      title: { text: 'Time' },
+    },
+    plotOptions: {
+      line: {
+        dataLabels: {
+          enabled: true,
+          format: '{point.name}',
+          allowOverlap: true,
+          defer: false,
+          // rotation: -30,
+          crop: false
+        },
+        marker: {
+          enabled: true,
+          radius: 3
+        }
+      },
+      series: {
+            states: {
+                hover: {
+                    enabled: false
+                }
+            }
+        }
     }
   }
 
   leaders.forEach(entry => {
     let dateFormatted = Date.UTC(entry.date.getUTCFullYear(), entry.date.getUTCMonth(), entry.date.getUTCDate(),
       entry.date.getUTCHours(), entry.date.getUTCMinutes())
-    chart.series[0].data.push([dateFormatted, entry.time])
+    chart.series[0].data.push({ x: dateFormatted, y: entry.time, name: entry.name, marker: { fillColor: hashColor(entry.name) } })
+    chart.series[0].zones.push({
+        value: dateFormatted,
+        color: hashColor(entry.name)
+    })
+  })
+
+  let lastEntry = leaders[leaders.length-1]
+  let lastEntryDateFormatted = Date.UTC(lastEntry.date.getUTCFullYear(), lastEntry.date.getUTCMonth()+1, lastEntry.date.getUTCDate())
+  chart.series[0].data.push({ x: lastEntryDateFormatted, y: lastEntry.time, name: lastEntry.name })
+  chart.series[0].zones.push({
+      color: hashColor(lastEntry.name)
   })
 
   return chart
 }
 
 // wc701
-let startDate = new Date('2017-02-19 19:00')
-let endDate = new Date('2017-02-26 17:00')
-let levelId = 371127
-let data = Object.values(JSON.parse(fs.readFileSync('wc701_data.json', 'utf8')))
+// let startDate = new Date('2017-02-19 19:00')
+// let endDate = new Date('2017-02-26 17:00')
+// let levelId = 371127
+// fetchData(levelId, (err, data) => {
+//   data = Object.values(JSON.parse(data))
+//   let chartData = produceChart(data, startDate, endDate, { title: '', levelId: levelId })
+//   writeHTML({ chart: JSON.stringify(chartData, null, 2) })
+// })
 
-let chartData = produceChart(data, startDate, endDate, { title: '', levelId: levelId })
-writeHTML({ chart: JSON.stringify(chartData, null, 2) })
+// wc702
+// let startDate = new Date('2017-02-26 17:00')
+// let endDate = new Date('2017-03-05 17:00')
+// let levelId = 371726
+// fetchData(levelId, (err, data) => {
+//   data = Object.values(JSON.parse(data))
+//   let chartData = produceChart(data, startDate, endDate, { title: '', levelId: levelId })
+//   writeHTML({ chart: JSON.stringify(chartData, null, 2) })
+// })
+
+// wc703
+// let startDate = new Date('2017-03-05 17:00')
+// let endDate = new Date('2017-03-12 18:00')
+// let levelId = 372197
+// fetchData(levelId, (err, data) => {
+//   data = Object.values(JSON.parse(data))
+//   let chartData = produceChart(data, startDate, endDate, { title: '', levelId: levelId })
+//   writeHTML({ chart: JSON.stringify(chartData, null, 2) })
+// })
+
+// wc704
+// let startDate = new Date('2017-03-12 18:00')
+// let endDate = new Date('2017-03-19 18:00')
+// let levelId = 372760
+// fetchData(levelId, (err, data) => {
+//   data = Object.values(JSON.parse(data))
+//   let chartData = produceChart(data, startDate, endDate, { title: '', levelId: levelId })
+//   writeHTML({ chart: JSON.stringify(chartData, null, 2) })
+// })
+
+// wc706
+// let startDate = new Date('2017-03-26 17:00')
+// let endDate = new Date('2017-04-02 17:00')
+// let levelId = 374120
+// fetchData(levelId, (err, data) => {
+//   data = Object.values(JSON.parse(data))
+//   let chartData = produceChart(data, startDate, endDate, { title: '', levelId: levelId })
+//   writeHTML({ chart: JSON.stringify(chartData, null, 2) })
+// })
+
+// wc707
+let startDate = new Date('2017-04-02 17:00')
+let endDate = new Date('2017-04-09 17:00')
+let levelId = 374907
+fetchData(levelId, (err, data) => {
+  data = Object.values(JSON.parse(data))
+  let chartData = produceChart(data, startDate, endDate, { title: '', levelId: levelId })
+  writeHTML({ chart: JSON.stringify(chartData, null, 2) })
+})
